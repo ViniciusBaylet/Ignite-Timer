23569a6bfb63233c4c16ef4c1ff03b5d0a6eb00c
ViniciusBaylet <viniciusbaylet6@gmail.com>
2025-08-26 17:43:34 -0300

    configurando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { ThemeProvider } from 'styled-components'
 import { Button } from "./components/Button/Button";
+
+import { defaultTheme } from './styles/theme/default';
 
 export function App() {
   
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
      <Button variant="primary" />
      <Button variant="secondary" />
      <Button variant="success" />
      <Button variant="danger" />
      <Button />
-    </>
+    </ThemeProvider>
   )
 }
 

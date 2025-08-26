de516aae8af97c0132159f613830403ff46774aa
ViniciusBaylet <viniciusbaylet6@gmail.com>
2025-08-26 17:27:55 -0300

    adicionando styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Button } from "./components/Button/Button";
 
 export function App() {
   
   return (
-    <h1>Hello world!</h1>
+    <>
+     <Button variant="primary" />
+     <Button variant="secondary" />
+     <Button variant="success" />
+     <Button variant="danger" />
+     <Button />
+    </>
   )
 }
 

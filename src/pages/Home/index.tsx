4a285c606fcb57ba3e74d4502faa8ffa515294a6
ViniciusBaylet <viniciusbaylet6@gmail.com>
2025-08-27 21:16:02 -0300

    validando formularios com zod

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,74 @@
+import { Play } from "lucide-react";
+import { useForm } from "react-hook-form";
+
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import zod from "zod";
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, "Informe a tarefa"),
+    minutesAmount: zod.number().min(1, "O ciclo precisa de no mínimo 1 minuto").max(60, "O ciclo precisa ser de no máximo 60 minutos")
+})
+
+export function Home() {
+
+    const { register, handleSubmit, watch } = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema)
+    });
+
+    function handleCreateNewCycle(data: any) {
+
+    }
+
+    const task = watch('task');
+    const isSubmitDisabled = !task;
+
+    return (
+        <HomeContainer>
+            <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <TaskInput
+                        id="task"
+                        placeholder="Dê um nome para o seu projeto" list="task-suggestions"
+                        {...register('task')}
+                    />
+
+                    <datalist id="task-suggestions">
+                        <option value="Projeto 1" />
+                        <option value="Projeto 2" />
+                        <option value="Projeto 3" />
+                        <option value="Projeto 4" />
+                    </datalist>
+
+                    <label htmlFor="minutesAmount">durante</label>
+                    <MinutesAmountInput
+                        type="number"
+                        id="minutesAmount" placeholder="00"
+                        step={5}
+                        min={1}
+                        max={60}
+                        {...register('minutesAmount')}
+                    />
+
+                    <span>minutos.</span>
+                </FormContainer>
+
+
+                <CountdownContainer>
+                    <span>0</span>
+                    <span>0</span>
+                    <Separator>:</Separator>
+                    <span>0</span>
+                    <span>0</span>
+                </CountdownContainer>
+
+                <StartCountdownButton type="submit" disabled={isSubmitDisabled} >
+                    <Play size={14} />
+                    Comerçar
+                </StartCountdownButton>
+            </form>
+        </HomeContainer>
+    )
+}

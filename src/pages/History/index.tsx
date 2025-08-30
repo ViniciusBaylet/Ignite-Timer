7df8c7e07955f7b998d6d2c2d39af44873eeb2b7
ViniciusBaylet <viniciusbaylet6@gmail.com>
2025-08-30 14:52:17 -0300

    Listagem do Histórico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -3,7 +3,7 @@ import { HistoryContainer, HistoryList, Status } from "./styles";
 import { CyclesContext } from "../../contexts/CyclesContext";
 
 export function History() {
-    
+
     const { cycles } = useContext(CyclesContext);
 
     return (
@@ -22,24 +22,22 @@ export function History() {
                     </thead>
 
                     <tbody>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusColor="green">Concluído</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusColor="yellow">Em andamento</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusColor="red">Interrompido</Status></td>
-                        </tr>
+                        {cycles.map(cycle => {
+                            return (
+                                <tr key={cycle.id}>
+                                    <td>{cycle.task}</td>
+                                    <td>{cycle.minutesAmount} minutos</td>
+                                    <td>{cycle.startDate.toISOString()}</td>
+                                    <td>
+                                        {cycle.finishedDate && <Status statusColor="green">Concluído</Status>}
+
+                                        {cycle.interruptDate && <Status statusColor="red">Interrompido</Status>}
+                                        
+                                        {!cycle.finishedDate  && !cycle.interruptDate && <Status statusColor="yellow">Em andamento</Status>}
+                                    </td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </table>
             </HistoryList>

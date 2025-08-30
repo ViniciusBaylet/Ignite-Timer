2d9e93a999c7f17f66511e71444c2021974c4265
ViniciusBaylet <viniciusbaylet6@gmail.com>
2025-08-30 14:59:12 -0300

    Formatação de Data

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 import { CyclesContext } from "../../contexts/CyclesContext";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale/pt-BR";
 
 export function History() {
 
@@ -27,13 +29,18 @@ export function History() {
                                 <tr key={cycle.id}>
                                     <td>{cycle.task}</td>
                                     <td>{cycle.minutesAmount} minutos</td>
-                                    <td>{cycle.startDate.toISOString()}</td>
+                                    <td>
+                                        {formatDistanceToNow(cycle.startDate, {
+                                            addSuffix: true,
+                                            locale: ptBR
+                                        })}
+                                    </td>
                                     <td>
                                         {cycle.finishedDate && <Status statusColor="green">Concluído</Status>}
 
                                         {cycle.interruptDate && <Status statusColor="red">Interrompido</Status>}
-                                        
-                                        {!cycle.finishedDate  && !cycle.interruptDate && <Status statusColor="yellow">Em andamento</Status>}
+
+                                        {!cycle.finishedDate && !cycle.interruptDate && <Status statusColor="yellow">Em andamento</Status>}
                                     </td>
                                 </tr>
                             )

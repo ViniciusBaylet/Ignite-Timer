cc01f493976c07e8d990734727e3a8dfe69248c7
ViniciusBaylet <viniciusbaylet6@gmail.com>
2025-08-31 10:31:51 -0300

    marcando ciclo como finalizado

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -41,28 +41,40 @@ export const CyclesContext = createContext({} as CyclesContextType);
 export function CyclesContextProvider({ children }: CyclesContextProviderProps) {
 
     const [cyclesState, dispatch] = useReducer((state: CyclesState, action: any) => {
-        if (action.type == 'ADD_NEW_CYCLE') {
-            return {
-                ...state,
-                cycles: [...state.cycles, action.payload.newCycle],
-                activeCycleId: action.payload.newCycle.id
-            };
+        switch (action.type) {
+            case 'ADD_NEW_CYCLE':
+                return {
+                    ...state,
+                    cycles: [...state.cycles, action.payload.newCycle],
+                    activeCycleId: action.payload.newCycle.id
+                }
+            case 'INTERRUPT_CURRENT_CYCLE':
+                return {
+                    ...state,
+                    cycles: state.cycles.map(cycle => {
+                        if (cycle.id == state.activeCycleId) {
+                            return { ...cycle, interruptDate: new Date() }
+                        } else {
+                            return cycle
+                        }
+                    }),
+                    activeCycleId: null
+                }
+            case 'MARK_CURRENT_CYCLE_AS_FINISHED':
+                return {
+                    ...state,
+                    cycles: state.cycles.map(cycle => {
+                        if (cycle.id == state.activeCycleId) {
+                            return { ...cycle, finishedDate: new Date() }
+                        } else {
+                            return cycle
+                        }
+                    }),
+                    activeCycleId: null
+                }
+            default:
+                return state
         }
-
-        if(action.type == 'INTERRUPT_CURRENT_CYCLE') {
-            return {
-                ...state,
-                cycles: state.cycles.map(cycle => {
-                    if(cycle.id == state.activeCycleId) {
-                        return { ...cycle, interruptDate: new Date() }
-                    } else {
-                        return cycle
-                    }
-                }),
-                activeCycleId: null
-            }
-        }
-        return state
     }, {
         cycles: [],
         activeCycleId: null
@@ -88,7 +100,7 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
                 newCycle
             }
         });
-        
+
         setAmountSecondsPassed(0);
     }
 

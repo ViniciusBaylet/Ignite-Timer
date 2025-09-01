8cf56ce698d2e3b73f7860e8b3e9eb593766436b
ViniciusBaylet <viniciusbaylet6@gmail.com>
2025-09-01 11:47:32 -0300

    salvando informações no local storage

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,6 +1,7 @@
-import { createContext, useState, type ReactNode, useReducer } from "react";
+import { createContext, useState, type ReactNode, useReducer, useEffect } from "react";
 import { cyclesReducer, type Cycle } from "../reducers/cycles/reducer";
 import { addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
+import { differenceInSeconds } from "date-fns";
 
 //useReducer -> funciona como o useState porém para armazenar informações mais complexas
 
@@ -31,13 +32,33 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
     const [cyclesState, dispatch] = useReducer(cyclesReducer, {
         cycles: [],
         activeCycleId: null
+    }, (initialState) => {
+        const storedStateAsJSON = localStorage.getItem('@ignite-timer:cycles-state-1.0.0');
+
+        if (storedStateAsJSON) {
+            return JSON.parse(storedStateAsJSON);
+        }
+
+        return initialState;
     });
 
-    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
-
     const { cycles, activeCycleId } = cyclesState;
-
     const activeCycle = cycles.find((cycle) => cycle.id == activeCycleId);
+
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+        if(activeCycle) {
+            return differenceInSeconds(new Date(), new Date(activeCycle.startDate));
+        }
+
+        return 0;
+    });
+
+    //Salvando informação no localStorage
+    useEffect(() => {
+        const stateJSON = JSON.stringify(cyclesState);
+
+        localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON);
+    }, [cyclesState]);
 
     function createNewCycle(data: CreateCycleData) {
         const newCycle: Cycle = {
--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -28,7 +28,7 @@ export function Countdown() {
         let interval: number;
         if (activeCycle) {
             interval = setInterval(() => {
-                const secondDifference = differenceInSeconds(new Date(), activeCycle.startDate);
+                const secondDifference = differenceInSeconds(new Date(), new Date(activeCycle.startDate));
 
                 if (secondDifference >= totalSeconds) {
                     markCurrentCycleAsFinished();
